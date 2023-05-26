9abcbeda0897d7bc42503016edf547ba230098e6
MelissaGodet <godetmelis@cy-tech.fr>
2023-05-26 22:24:52 +0200

    SignInController

--- web-app/src/main/java/hr/algebra/tracefood/webapp/controller/SignInController.java
+++ web-app/src/main/java/hr/algebra/tracefood/webapp/controller/SignInController.java
@@ -1,35 +1,75 @@
 package hr.algebra.tracefood.webapp.controller;
 
-import hr.algebra.tracefood.webapp.model.Distributor;
-import hr.algebra.tracefood.webapp.model.User;
+
+import hr.algebra.tracefood.webapp.model.*;
 import hr.algebra.tracefood.webapp.service.DistributorService;
 import hr.algebra.tracefood.webapp.service.ProcessorService;
 import hr.algebra.tracefood.webapp.service.ProducerService;
 import hr.algebra.tracefood.webapp.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class SignInController {
 
     @Autowired
     public ProcessorService processorService;
+
+    @Autowired
     public DistributorService distributorService;
+
+    @Autowired
     public ProducerService producerService;
+
+    @Autowired
     public SellerService sellerService;
 
+    public final List<Processor> processors = processorService.getAll();
+    public final List<Distributor> distributors = distributorService.getAll();
+    public final List<Producer> producers = producerService.getAll();
+    public final List<Seller> sellers = sellerService.getAll();
+
     @GetMapping("/signIn")
-    public String signIn() {
+    public String signIn(Model model) {
+        model.addAttribute("error", false);
         return "signIn";
     }
 
     @PostMapping("/login")
-    public String SignIn(@RequestParam("companyName") String companyName,
-                                        @RequestParam("emailAddress") String emailAddress) {
-        return "signUpDistributor"; //TODO retourner la bonne page
+    public String SignIn(@RequestParam("emailAddress") String emailAddress, @RequestParam("password") String password, Model model) {
+        boolean connexionSuccessful = false;
+        for (Processor processor : processors) {
+            if (processor.getEmailAddress().equals(emailAddress) && processor.getPassword().equals(password)) {
+                connexionSuccessful = true;
+            }
+        }
+        for (Distributor distributor : distributors) {
+            if (distributor.getEmailAddress().equals(emailAddress) && distributor.getPassword().equals(password)) {
+                connexionSuccessful = true;
+            }
+        }
+        for (Producer producer : producers) {
+            if (producer.getEmailAddress().equals(emailAddress) && producer.getPassword().equals(password)) {
+                connexionSuccessful = true;
+            }
+        }
+        for (Seller seller : sellers) {
+            if (seller.getEmailAddress().equals(emailAddress) && seller.getPassword().equals(password)) {
+                connexionSuccessful = true;
+            }
+        }
+        if (connexionSuccessful) {
+            model.addAttribute("error", true);
+            return "redirect:/signUpDistributor"; //TODO retourner la bonne page
+        } else {
+            return "signUpDistributor"; //TODO retourner la bonne page
+        }
     }
 
 }
