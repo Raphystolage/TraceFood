35a0721acf55d861162e08987906440c921e6567
MelissaGodet <godetmelis@cy-tech.fr>
2023-05-30 22:25:22 +0200

    update SignController page and User controller page

--- web-app/src/main/java/hr/algebra/tracefood/webapp/controller/SignController.java
+++ web-app/src/main/java/hr/algebra/tracefood/webapp/controller/SignController.java
@@ -1,10 +1,8 @@
 package hr.algebra.tracefood.webapp.controller;
 
 import hr.algebra.tracefood.webapp.model.*;
-/* import hr.algebra.tracefood.webapp.service.HoReCaService; */
-import hr.algebra.tracefood.webapp.service.ProcessorService;
-import hr.algebra.tracefood.webapp.service.ProducerService;
-import hr.algebra.tracefood.webapp.service.SellerService;
+
+import hr.algebra.tracefood.webapp.service.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +18,6 @@ import java.util.List;
 @Controller
 public class SignController {
 
-/*
     @Autowired
     private HttpServletRequest request;
 
@@ -74,26 +71,25 @@ public class SignController {
     public String signUpSeller() {
         return "signUpSeller";
     }
-/*
+
     @PostMapping("/signUpSeller")
     public String SignUpProcessorForm(@RequestParam("companyName") String companyName,
                                     @RequestParam("companyAddress") String companyAddress,
-                                    @RequestParam("type") String type, //TODO changer le type en enum
+                                    @RequestParam("type") SellerType type,
                                     @RequestParam("emailAddress") String emailAddress,
-                                    @RequestParam("certifications") List<String> certifications,
                                     @RequestParam("password") String password, Model model) {
 
         boolean signUpSuccessful = true;
 
         for (Seller seller : sellers) {
-            if (seller.getEmailAddress().equals(emailAddress)) {
+            if (seller.getUser().getEmailAddress().equals(emailAddress)) {
                 signUpSuccessful = false;
             }
         }
 
         if (signUpSuccessful) {
             HttpSession session = request.getSession();
-            User newUser = new User(emailAddress, password, companyName, certifications, companyAddress);
+            User newUser = new User(emailAddress, password, companyName, companyAddress);
             Seller newSeller = new Seller(newUser, type;
             sellerService.create(newSeller);
             session.setAttribute("user", newSeller);
@@ -104,29 +100,28 @@ public class SignController {
             model.addAttribute("error", true);
 
             return "signUpProcessor";
+        }
 
     }
 
     @PostMapping("/signUpProcessor")
-
     public String SignUpProcessorForm(@RequestParam("companyName") String companyName,
                                     @RequestParam("companyAddress") String companyAddress,
-                                    @RequestParam("type") String type, //TODO changer le type en enum
+                                    @RequestParam("type") ProcessorType type,
                                     @RequestParam("emailAddress") String emailAddress,
-                                      @RequestParam("certifications") List<String> certifications,
                                     @RequestParam("password") String password, Model model) {
 
         boolean signUpSuccessful = true;
 
         for (Processor processor: processors) {
-            if (processor.getEmailAddress().equals(emailAddress)) {
+            if (processor.getUser().getEmailAddress().equals(emailAddress)) {
                 signUpSuccessful = false;
             }
         }
 
         if (signUpSuccessful) {
             HttpSession session = request.getSession();
-            User newUser = new User(emailAddress, password, companyName, certifications, companyAddress);
+            User newUser = new User(emailAddress, password, companyName, companyAddress);
             Processor newProcessor = new Processor(newUser, type);
             processorService.create(newProcessor);
             session.setAttribute("user", newProcessor);
@@ -144,22 +139,21 @@ public class SignController {
     @PostMapping("/signUpProducer")
     public String SignUpProducerForm(@RequestParam("companyName") String companyName,
                                     @RequestParam("companyAddress") String companyAddress,
-                                    @RequestParam("type") String type, //TODO changer le type en enum
-                                     @RequestParam("certifications") List<String> certifications,
+                                    @RequestParam("type") ProducerType type,
                                     @RequestParam("emailAddress") String emailAddress,
                                     @RequestParam("password") String password, Model model) {
 
         boolean signUpSuccessful = true;
 
         for (Producer producer: producers) {
-            if (producer.getEmailAddress().equals(emailAddress)) {
+            if (producer.getUser().getEmailAddress().equals(emailAddress)) {
                 signUpSuccessful = false;
             }
         }
 
         if (signUpSuccessful) {
             HttpSession session = request.getSession();
-            User newUser = new User(emailAddress, password, companyName, certifications, companyAddress);
+            User newUser = new User(emailAddress, password, companyName, companyAddress);
             Producer newProducer = new Producer(newUser, type);
             producerService.create(newProducer);
             session.setAttribute("user", newProducer);
@@ -178,25 +172,24 @@ public class SignController {
     public String SignUpHoReCaForm(@RequestParam("companyName") String companyName,
                                     @RequestParam("emailAddress") String emailAddress,
                                     @RequestParam("companyAddress") String companyAddress,
-                                   @RequestParam("certifications") List<String> certifications,
                                     @RequestParam("password") String password,
-                                   @RequestParam("type") String type, //TODO changer le type en enum
+                                   @RequestParam("type") HoReCaType type,
                                    Model model) {
 
         boolean signUpSuccessful = true;
 
         for (HoReCa hoReCa: hoReCas) {
-            if (hoReCa.getEmailAddress().equals(emailAddress)) {
+            if (hoReCa.getUser().getEmailAddress().equals(emailAddress)) {
                 signUpSuccessful = false;
             }
 
         }
         if (signUpSuccessful) {
             HttpSession session = request.getSession();
-            User newUser = new User(emailAddress, password, companyName, certifications, companyAddress);
+            User newUser = new User(emailAddress, password, companyName, companyAddress);
             HoReCa newHoReCa = new HoReCa(newUser, type) ;
             hoReCaService.create(newHoReCa);
-            session.setAttribute("user", newProducer);
+            session.setAttribute("user", newHoReCa);
 
             return "redirect:/userHomePage";
         } else {
@@ -213,12 +206,9 @@ public class SignController {
     public String SignIn(@RequestParam("emailAddress") String emailAddress, @RequestParam("password") String password, Model model) {
 
         boolean connexionSuccessful = false;
-        List<User> users = new ArrayList<>();
+        UserService userService = new UserService();
+        List<User> users = userService.getAll();
         User rightUser = null;
-
-        users.addAll(processors);
-        users.addAll(producers);
-        users.addAll(sellers);
 
         for (User user : users) {
             if (user.getEmailAddress().equals(emailAddress) && user.getPassword().equals(password)) {
@@ -229,8 +219,7 @@ public class SignController {
 
         if (connexionSuccessful) {
             HttpSession session = request.getSession();
-            UserService userService = new UserService();
-            switch (user.getClass().getSimpleName()) {
+            switch (rightUser.getClass().getSimpleName()) {
                 case "Seller":
                     session.setAttribute("user", userService.getSellerByUserId(rightUser.getId()));
                     break;
@@ -241,7 +230,7 @@ public class SignController {
                    session.setAttribute("user", userService.getProducerByUserId(rightUser.getId()));
                     break;
                 case "HoReCa":
-                    session.setAttribute("user", userService.getHoReCaByUserId(rightUser.getId());
+                    session.setAttribute("user", userService.getHoReCaByUserId(rightUser.getId()));
                     break;
                 default:
                     System.out.println("User type not recognized.");
@@ -255,6 +244,5 @@ public class SignController {
             return "signUpHoReCa";
         }
     }
-*/
 
 }
